fde70bd43ebe113a17941da95f1738d97399644a
Samuel Carriere <samrcarr@amazon.com>
2021-12-06 00:01:37 -0800

    day4 second section

--- src/2021/day4.ts
+++ src/2021/day4.ts
@@ -27,6 +27,7 @@ interface Board {
   data: Record<string, [number, number]>;
   score: [number[], number[]];
   sum: number;
+  won?: boolean;
 }
 
 function createBoards(input: Array<string>): Board[] {
@@ -68,6 +69,32 @@ function createBoards(input: Array<string>): Board[] {
   return boards;
 }
 
-function part2(input: Array<string>) {}
+function part2(input: Array<string>) {
+  const randomSeq = input[0].split(",");
+  const boards = createBoards(input);
+  let winningBoardsCount = 0;
+  for (let numberDraw of randomSeq) {
+    for (let board of boards) {
+      if (board.won) {
+        continue;
+      }
+      if (board.data[numberDraw]) {
+        ++board.score[0][board.data[numberDraw][0]];
+        ++board.score[1][board.data[numberDraw][1]];
+        board.sum -= parseInt(numberDraw);
+        if (
+          board.score[0][board.data[numberDraw][0]] === 5 ||
+          board.score[1][board.data[numberDraw][1]] === 5
+        ) {
+          board.won = true;
+          ++winningBoardsCount;
+          if (winningBoardsCount === boards.length) {
+            return board.sum * parseInt(numberDraw);
+          }
+        }
+      }
+    }
+  }
+}
 
 export { part1, part2 };
